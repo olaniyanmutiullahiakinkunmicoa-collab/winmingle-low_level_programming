0d17fe3c51a21239d114457afe54f819b23a5661
coachakin <olaniyan@DESKTOP-R7ML5UE.localdomain>
2026-05-12 23:03:01 +0100

    Fix submodule issue

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (void)
+
+
+{ 
+
+printf("the size of a char is %zu\n", sizeof(char));
+
+printf("the size of an int is %zu\n", sizeof(int));
+
+printf("the size of an long int is %zu\n", sizeof(long int));
+
+printf("the size of an double int is %zu\n",sizeof(long long int));
+
+printf("the size of an float is %zu\n", sizeof(float));
+
+return 0;
+}
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+/*
+ * Author: Olaniyan Mutiullahi Akin
+ * Program: WinMingle Community C Traininig
+ * Assignment on runing a C script in preprocessor manner(Hello,world)
+ */
+int main (void)
+
+{
+ printf("Programming C is fun");
+
+return 0;
+
+
+}
