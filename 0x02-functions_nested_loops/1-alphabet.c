430fa334d599f938eca4badf065fd885e6dfe730
coachakin <olaniyan@DESKTOP-R7ML5UE.localdomain>
2026-05-24 21:41:43 +0100

    function def of  alphabet lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+ /**
+ * Author: Olaniyan Mutiullahi Akin
+ * Program: WinMingle Community C Training
+ * Description: fuction definition of lowercase alphabet
+ */
+
+void print_alphabet(void) 
+{
+    int x;
+    
+    for( x = 'a'; x <= 'z'; ++x)
+        { _putchar (x);
+        }
+        _putchar ('\n');
+}
+
