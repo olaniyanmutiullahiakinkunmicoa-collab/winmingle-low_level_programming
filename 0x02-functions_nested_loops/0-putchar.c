26c469dc091a0d62047c28fc1995f7ac49b151c0
coachakin <olaniyan@DESKTOP-R7ML5UE.localdomain>
2026-05-24 21:04:25 +0100

    the function containing file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/*Name :Olaniyan Mutiullahi Akin
+Topic: function and nested loop
+school: winmingle */
+
+int main (void) 
+{
+    char a[] = {"_putchar"};
+    int i = 0;
+    char x;
+    while ( i<= 8)
+     {   x = a[i];
+        _putchar(x);
+        ++i;}
+     putchar ('\n');
+
+    return 0;
+}
