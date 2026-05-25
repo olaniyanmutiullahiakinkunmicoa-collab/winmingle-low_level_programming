c90a6b1067f63ec83a750fc01d0444f261585c6f
coachakin <olaniyan@DESKTOP-R7ML5UE.localdomain>
2026-05-25 08:03:18 +0100

    is the argument lowercase or upper?

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * Author: Olaniyan Mutiullahi Akin
+ * Program: WinMingle Community C Training
+ * Description: fuction definition of lowercase alphabet
+ */
+
+int _islower(int c)
+{
+     
+    if(c >= 'a' &&  c <= 'z')
+     {  return 1;
+     }
+        else 
+         {   return 0;
+      
+         }
+
+         
+
+
+return 0;
+}
