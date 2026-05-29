0257b441c4ef7e2342b4097f3b4529dfef30071b
coachakin <olaniyan@DESKTOP-R7ML5UE.localdomain>
2026-05-29 11:00:00 +0100

    displaying time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+void times_table(void)
+{
+    int row, col, result;
+    for (row = 0; row <= 9; ++row)
+    {     
+        for (col = 0; col <= 9; ++col)
+        {
+            result = row * col;
+            if (col == 0) 
+            {     
+              _putchar (result + '0');
+            }           
+
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+            
+            
+                if (result < 10) 
+                {
+                    _putchar(' ');
+                    _putchar (result + '0');                
+                }
+                else 
+                {
+                    _putchar((result / 10) + '0'); 
+                    _putchar((result % 10) + '0'); 
+                }    
+        
+        
+            }
+
+               
+        }
+            _putchar ('\n');
+
+    }
+
+}
