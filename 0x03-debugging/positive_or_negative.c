3cda14c46b6f5b785217fff8647c431e709f1678
coachakin <olaniyan@DESKTOP-R7ML5UE.localdomain>
2026-06-10 13:25:36 +0100

    function of  positive_or_negative number display zero as input

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Author: Olaniyan Mutiullahi Akin
+ * Program: WinMingle Community C Training
+ * Description: Deburging of main.h and 0-main
+ */
+
+void positive_or_negative(int i) 
+{
+    if (i < 0) 
+    {
+        printf("-1\n");
+    }
+
+    else if (i > 0) 
+    {
+        printf("1\n");
+    }
+
+    else
+    {
+        printf(" %i is zero \n", i);
+    }
+
+    
+
+
+}
