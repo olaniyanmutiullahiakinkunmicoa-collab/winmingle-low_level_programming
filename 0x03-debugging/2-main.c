4afce78ea98192941c96df9f966aa4544a049749
coachakin <olaniyan@DESKTOP-R7ML5UE.localdomain>
2026-06-11 16:48:27 +0100

    main.c of  file 2-largest_number.c

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+int main (void) 
+{
+    int largest;    
+    largest = largest_number( 2, 100, 972);
+
+    printf("%i is the largest number\n", largest);
+    return 0;
+}
