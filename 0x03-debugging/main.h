b8593daca7ec03e9a763936fc195ebad74c064f9
coachakin <olaniyan@DESKTOP-R7ML5UE.localdomain>
2026-06-11 15:47:02 +0100

    main.h of all codes

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,4 +4,10 @@
  * Description:creating "main.h" (Deburging) 
  */
 
+#ifndef MAIN_H
+#define MAIN_H
+
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+
+#endif /* MAIN_H */
