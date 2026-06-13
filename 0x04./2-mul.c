444da2da2396cfe4b542c1766924162ac32e5392
coachakin <olaniyan@DESKTOP-R7ML5UE.localdomain>
2026-06-13 14:39:32 +0100

    function for two int multiplication

---
+++ 0x04./2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - causes an infinite loop
+ * Author: Olaniyan Mutiullahi Akin
+ * Program: WinMingle Community C Training
+ * Description: Deburging cause of infinite loop
+ */
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of a * b
+ */
+int mul(int a, int b)
+{
+    return (a * b);
+}
