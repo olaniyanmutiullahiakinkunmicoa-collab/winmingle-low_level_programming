720fcb6a920687da3e77fc55d68860da93e174c1
coachakin <olaniyan@DESKTOP-R7ML5UE.localdomain>
2026-06-15 12:54:40 +0100

    function of number from 0 to 9

---
+++ 0x04./3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - causes an infinite loop
+ * Author: Olaniyan Mutiullahi Akin
+ * Program: WinMingle Community C Training
+ * Description: funtion to print from 0 to 9
+ */
+void print_numbers(void)
+{
+    char  c;
+  for (c = '0'; c <= '9'; ++c) 
+       { _putchar (c  );
+       }
+        _putchar ('\n');
+}
