850664c5dbc11f81f366744a9c4d7ab65c41f359
coachakin <olaniyanmutiullahiakinkunmicoa@gmail.com>
2026-06-25 21:34:05 +0100

    number interchange

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* main - causes an infinite loop
+ * Author: Olaniyan Mutiullahi Akin
+ * Program: WinMingle Community C Training
+ * Description: function to interchange, using pointer  
+*/
+
+void swap_int(int *a, int *b) 
+{
+   int temp;
+     temp = *a;
+    
+    *a = *b;
+
+    *b = temp;
+    
+    
+}
+
+
