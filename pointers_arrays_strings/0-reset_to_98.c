568324e6974484f5b8c0777ce3c63b507a967369
coachakin <olaniyanmutiullahiakinkunmicoa@gmail.com>
2026-06-25 18:18:01 +0100

    pointer value change

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* main - causes an infinite loop
+ * Author: Olaniyan Mutiullahi Akin
+ * Program: WinMingle Community C Training
+ * Description: pointer value change
+
+
+**
+ * reset_to_98 - updates the value pointed to by n to 98
+ * @n: pointer to an integer
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+
+    *n = 98;
+
+}
