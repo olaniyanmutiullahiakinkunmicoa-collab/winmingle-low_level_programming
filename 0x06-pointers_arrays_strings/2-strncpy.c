cce5937a0bbd10bad31c1ecef40f8fea9d83f4b7
coachakin <olaniyanmutiullahiakinkunmicoa@gmail.com>
2026-07-09 19:34:12 +0100

    copy two strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+
+
+/**
+* main - causes an infinite loop
+ * Author: Olaniyan Mutiullahi Akin
+ * Program: WinMingle Community C Training
+ * Description: function to  two strings
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
