e2dc5c7898a5440693ca999702ba5688063db213
coachakin <olaniyanmutiullahiakinkunmicoa@gmail.com>
2026-08-13 00:25:11 +0100

    the bash file

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cp ../0x02-functions_nested_loops/_putchar.c .
+cp ../0x02-functions_nested_loops/3-islower.c .
+cp ../0x02-functions_nested_loops/4-isalpha.c .
+cp ../0x02-functions_nested_loops/6-abs.c .
+
+cp ../0x04./0-isupper.c .
+cp ../0x04./1-isdigit.c .
+
+cp ../pointers_arrays_strings/2-strlen.c .
+cp ../pointers_arrays_strings/3-puts.c .
+cp ../pointers_arrays_strings/9-strcpy.c .
+cp ../pointers_arrays_strings/10-atoi.c .
+
+cp ../0x06-pointers_arrays_strings/0-strcat.c .
+cp ../0x06-pointers_arrays_strings/1-strncat.c .
+cp ../0x06-pointers_arrays_strings/2-strncpy.c .
+cp ../0x06-pointers_arrays_strings/3-strcmp.c .
+
+cp ../0x07-pointers_arrays_strings/0-memset.c .
+cp ../0x07-pointers_arrays_strings/_memcpy.c .
+cp ../0x07-pointers_arrays_strings/2-strchr.c .
+cp ../0x07-pointers_arrays_strings/3-strspn.c .
+cp ../0x07-pointers_arrays_strings/4-strpbrk.c .
+cp ../0x07-pointers_arrays_strings/5-strstr.c .
