9894437f10b1599b78c7384fbebcd688ba4ccf2d
coachakin <olaniyanmutiullahiakinkunmicoa@gmail.com>
2026-08-14 20:12:49 +0100

    program that prints its own name.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+
+School:Winmingle
+Topics:Write a C program that prints its own name
+
+*/
+
+
+int main(int argc, char *argv[])
+
+{
+    (void)argc;
+
+    printf("the name of the file  is %s\n",argv[0]);
+
+}
